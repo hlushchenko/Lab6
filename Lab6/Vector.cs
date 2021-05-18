fef98b855cfcf9dfef2574e572d9b0b5e915519f
Hluushchenko <Hluushchenko@gmail.com>
2021-05-18 20:21:16 +0300

    Ray and vector added

---
+++ Lab6/Vector.cs
@@ -0,0 +1,23 @@
+namespace Lab6
+{
+    public class Vector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        
+        public Vector(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vector(Point start, Point end)
+        {
+            X = end.X - start.X;
+            Y = end.Y - start.Y;
+            Z = end.Z - start.Z;
+        }
+    }
+}
