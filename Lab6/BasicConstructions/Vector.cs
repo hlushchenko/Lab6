b485385997c13bee0e27e367f07642ca7e48dd6c
Hluushchenko <Hluushchenko@gmail.com>
2021-05-18 22:45:48 +0300

    CrossProduct and DotProduct added

--- Lab6/BasicConstructions/Vector.cs
+++ Lab6/BasicConstructions/Vector.cs
@@ -19,5 +19,20 @@ namespace Lab6.BasicConstructions
             Y = end.Y - start.Y;
             Z = end.Z - start.Z;
         }
+        
+        ///<summary>Векторний добуток векторів</summary>
+        public Vector CrossProduct(Vector vector)
+        {
+            float i = Y * vector.Z - Z * vector.Y;
+            float j = Z * vector.X - X * vector.Z;
+            float k = X * vector.Y - Y * vector.X;
+            return new Vector(i, j, k);
+        }
+        
+        ///<summary>Скалярний добуток векторів</summary>
+        public float DotProduct(Vector vector)
+        {
+            return X * vector.X + Y * vector.Y + Z * vector.Z;
+        }
     }
 }
