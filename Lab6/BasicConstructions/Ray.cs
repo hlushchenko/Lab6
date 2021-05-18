f2883ec1ec1aba1f2db28ee9dc80b38647dd66a1
Hluushchenko <Hluushchenko@gmail.com>
2021-05-18 23:47:25 +0300

    ray intersection added

--- Lab6/BasicConstructions/Ray.cs
+++ Lab6/BasicConstructions/Ray.cs
@@ -1,14 +1,55 @@
+using Lab6.BasicConstructions.Mesh;
+
 namespace Lab6.BasicConstructions
 {
     public class Ray
     {
         public Vector Direction;
-        public Point Base;
+        public Point Origin;
 
-        public Ray(Vector direction, Point @base)
+        public Ray(Vector direction, Point origin)
         {
             Direction = direction;
-            Base = @base;
+            Origin = origin;
+        }
+        
+        ///<summary>Перевірка перетину променя з полігоном</summary>
+        public bool Intersects(Triangle triangle, ref Point outPoint)
+        {
+            const double eps = 0.0000001;
+            Vector edge1 = new Vector(triangle.Verticles[0], triangle.Verticles[1]);
+            Vector edge2 = new Vector(triangle.Verticles[0], triangle.Verticles[2]);
+            Vector h = Direction.CrossProduct(edge2);
+            float a = edge1.DotProduct(h);
+            if (a > -eps && a < eps)
+            {
+                return false;
+            }
+            
+            float f = 1f / a;
+            Vector s = new Vector(triangle.Verticles[0], Origin);
+            float u = f * s.DotProduct(h);
+            
+            if (u < 0f || u > 1f)
+            {
+                return false;
+            }
+
+            Vector q = s.CrossProduct(edge1);
+            float v = f * Direction.DotProduct(q);
+
+            if (v < 0f || u + v > 1f)
+            {
+                return false;
+            }
+
+            float t = f * edge2.DotProduct(q);
+            if (t > eps)
+            {
+                outPoint = Origin + Direction * t;
+                return true;
+            }
+            return false;
         }
     }
 }
--- Lab6/BasicConstructions/Vector.cs
+++ Lab6/BasicConstructions/Vector.cs
@@ -2,10 +2,11 @@ namespace Lab6.BasicConstructions
 {
     public class Vector
     {
+        public static Vector Zero => new (0, 0, 0);
         public float X { get; set; }
         public float Y { get; set; }
         public float Z { get; set; }
-        
+
         public Vector(float x, float y, float z)
         {
             X = x;
@@ -34,5 +35,28 @@ namespace Lab6.BasicConstructions
         {
             return X * vector.X + Y * vector.Y + Z * vector.Z;
         }
+
+        #region Arythmetic operations
+        public static Vector operator *(Vector vector, float mult)
+        {
+            return new(vector.X * mult, vector.Y * mult, vector.Z * mult);
+        }
+        
+        public static Vector operator /(Vector vector, float div)
+        {
+            return new(vector.X / div, vector.Y / div, vector.Z / div);
+        }
+        
+        public static Vector operator +(Vector vector1, Vector vector2)
+        {
+            return new(vector1.X + vector2.X, vector1.Y + vector2.Y, vector1.Z + vector2.Z);
+        }
+
+        public static Vector operator -(Vector vector1, Vector vector2)
+        {
+            return new(vector1.X - vector2.X, vector1.Y - vector2.Y, vector1.Z - vector2.Z);
+        }
+        #endregion
+        
     }
 }
