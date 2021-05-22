5c8b7a377cfd0a915b67999358e5acf3d2f51111
ASharp <a.sharp.fus@gmail.com>
2021-05-22 17:27:39 +0300

    bug with enormous light is fixed
    hurray, comrades!!!

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -12,10 +12,10 @@ namespace Lab6
         static void Main(string[] args)
         {
             string resourcesPath = @"..\..\..\Resources\";
-            int resx = 10;
-            int resy = 10;
+            int resx = 80;
+            int resy = 80;
             Camera cum = new Camera(60, resx, resy, 0, -3, 0, new Vector(0, 0.90f, 0.1f));
-            Light lite = new Light(new Point(0, -2, 0), 5, Color.White);
+            Light lite = new Light(new Point(2, 0, -3), 20, Color.White);
             Mesh meh = new Mesh(resourcesPath + "Sphere.obj");
             Scene mainScene = new Scene(cum, lite, meh);
             cum.Screenshot(resourcesPath + "result.bmp");
